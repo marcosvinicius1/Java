c7cd8fbeb34d8dd94f296e968ccb7e85869ec627
marcosvinicius1 <marcos_rv@outlook.com>
2016-10-24 13:57:57 -0200

    V-1.3.12 BETA 24102016

--- SAC/src/br/com/atmatech/sac/view/ViewLogin.java
+++ SAC/src/br/com/atmatech/sac/view/ViewLogin.java
@@ -811,8 +811,8 @@ public class ViewLogin extends javax.swing.JFrame {
         // TODO add your handling code here:
         try{
         jTusuario.requestFocusInWindow();
-        jPanel1.setBorder(BorderFactory.createTitledBorder("LOGIN V-1.3.11 BETA"));
-        if(!new Versao().verificaVersao(1,3,11)){
+        jPanel1.setBorder(BorderFactory.createTitledBorder("LOGIN V-1.3.12 BETA"));
+        if(!new Versao().verificaVersao(1,3,12)){
             jButton1.setEnabled(false);
             JOptionPane.showMessageDialog(null, "Versão Incompativel com Servidor");
             try {
